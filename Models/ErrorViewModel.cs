b583d055e9725a76d7f24638d929085282b30770
MarioAlabi <marioedgardovilledaalabi@gmail.com>
2026-08-17 11:27:20 -0600

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MonitoringApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
